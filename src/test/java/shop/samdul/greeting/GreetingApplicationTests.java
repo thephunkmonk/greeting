379be03b7d717ab18d358c3d759690ca12487fbe
dMario24 <data.mario24@gmail.com>
2024-10-15 16:17:30 +0900

    init

---
+++ src/test/java/shop/samdul/greeting/GreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.samdul.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
