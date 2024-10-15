d6710e626373c68dd14abf460214f514dffe9ff6
dMario24 <data.mario24@gmail.com>
2024-10-15 17:22:52 +0900

    Controller

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Getting Started
 
+### Run
+```bash
+$ ./gradlew bootRun
+```
+
+```bash
+$ ./gradlew build
+$ java -jar build/libs/greeting-0.0.1-SNAPSHOT.jar
+```
+
 ### Reference Documentation
 For further reference, please consider the following sections:
 
---
+++ src/main/java/shop/samdul/greeting/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package shop.samdul.greeting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+  
+  @GetMapping("/greeting")	
+  public String greeting(
+		  @RequestParam(
+		  	name="name",
+			required=false,
+			defaultValue="HI") String name,
+	  	  Model model) {
+	
+	 return "greeting";
+		  
+  }
+}
