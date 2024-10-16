4d6aeefd3fb41b4ead2bf8fbd2e2d8688e96b5de
dMario24 <data.mario24@gmail.com>
2024-10-16 14:03:17 +0900

    abc

--- src/main/java/shop/samdul/greeting/GreetingApplication.java
+++ src/main/java/shop/samdul/greeting/GreetingApplication.java
@@ -9,5 +9,4 @@ public class GreetingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingApplication.class, args);
 	}
-
 }
--- src/main/java/shop/samdul/greeting/controller/GreetingController.java
+++ src/main/java/shop/samdul/greeting/controller/GreetingController.java
@@ -5,20 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import shop.samdul.greeting.model.People;
+
+import java.util.Stack;
+
 @Controller
 public class GreetingController {
+
+	private Stack<People> nameStack = new Stack<>();
   
   @GetMapping("/greeting")	
-  public String greeting(
-		  @RequestParam(
-		  	name="name",
-			required=false,
-			defaultValue="HI") String name,
-	  	  Model model) {
-	 
-	 model.addAttribute("name", name);
-	 
-	 return "greeting";
-		  
+  public String greeting(@RequestParam(name="name", required=false, defaultValue="HI") String name, Model model) {
+		People p = new People();
+		if (!nameStack.isEmpty()) {
+			People pp = nameStack.peek();
+			p.setNum(pp.getNum() + 1);
+		} else {
+				p.setNum(1);
+		}
+		p.setName(name);
+		nameStack.push(p);
+		model.addAttribute("nameStack", nameStack);
+		
+		return "greeting";
   }
 }
