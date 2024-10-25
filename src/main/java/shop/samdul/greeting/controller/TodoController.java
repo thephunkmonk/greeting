0b9238166c4492400f4a24f1f1f7e70f5364d8da
thephunkmonk <andrewchoi1218@gmail.com>
2024-10-25 09:32:06 +0900

    i was in mid thingy

--- src/main/java/shop/samdul/greeting/controller/TodoController.java
+++ src/main/java/shop/samdul/greeting/controller/TodoController.java
@@ -1,8 +1,11 @@
 package shop.samdul.greeting.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import shop.samdul.greeting.service.TodoService;
@@ -29,9 +32,22 @@ public class TodoController{
 		return r;	
 	}
 
-	//C
+	//C - INSERT
+	@PostMapping
+	public void insert(TodoEntity todoEntity){
+		TodoEntity createdTodo = todoService.createEntity(todoEntity);
 
-	//U
-
-	//D
+	}
+	//U - UPDATE
+	@PutMapping("todos/{id}")
+	public void update(int id, TodoEntity todoEntity){
+		todoEntity.setId(id);
+		todoService.updateById(todoEntity);
+	}
+	//D - DELETE
+	@DeleteMapping("todos/{id}")
+	public void delete(int id){
+		todoService.deleteById(id);
+        System.out.println(id + " delete success");
+	}
 }
